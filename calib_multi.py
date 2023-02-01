e466f270356897cebe04131dbeacde2fab285dc5
kakghiroshi <77578976+kakghiroshi@users.noreply.github.com>
2023-02-01 15:27:13 +0800

    Update calib_multi.py

--- calib_multi.py
+++ calib_multi.py
@@ -105,6 +105,13 @@ if __name__ == "__main__":
         img=cv2.imread(img_path)
         # 主要功能是截取点云
         if cropped_flag == True:
+            print("1) Press 'Y' twice to align geometry with negative direction of y-axis")
+            print("2) Press 'K' to lock screen and to switch to selection mode")
+            print("3) Drag for rectangle selection,")
+            print("   or use ctrl + left click for polygon selection")
+            print("4) Press 'C' to get a selected geometry")
+            print("5) Press 'S' to save the selected geometry")
+            print("6) Press 'F' to switch to freeview mode")
             pcd = o3d.io.read_point_cloud(pcd_path)
             o3d.visualization.draw_geometries_with_editing([pcd])
         #加载截取后的点云
