9c03141c6d56684049c7cafe2b1b842e1ed0a72e
kakghiroshi <77578976+kakghiroshi@users.noreply.github.com>
2023-02-02 17:05:51 +0800

    Update calib_multi.py

--- calib_multi.py
+++ calib_multi.py
@@ -34,8 +34,8 @@ def calib():
 
     rotM = cv2.Rodrigues(rotation_vector)[0]
     position = -np.matrix(rotM).T * np.matrix(translation_vector)
-    print(rotM)
-    print("Lidar to camera rotation:",rotM.T)
+    print("Lidar to camera rotation:",rotM)
+    print(rotM.T)
     print(position)
     # print("file Vector:\n {0}".format(-np.matrix(rotM).T * np.matrix(translation_vector)))
 
